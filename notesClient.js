cac5bfb3092196093c7f3fc822cc131d7b2bbe97
rachelwall <rachelwall@live.co.uk>
2023-09-15 13:39:40 +0100

    adding delete stored data function

--- notesClient.js
+++ notesClient.js
@@ -9,20 +9,41 @@ class NotesClient{
       };
     
       createNote(note, callback) {
+        ///note has format {content: 'this ia s note'} to match the server`POST method which is looking for the request to have req.body.content attribute 
         const requestBody = JSON.stringify(note);
+        ///stringify to convert to a json string as expected by the POST method
         return fetch("http://localhost:3000/notes", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
+          
           body: requestBody
         })
           .then(response => response.json())
+          ///response is the array with the posted note added as can be seen on backend app
           .then(data => {
               console.log(data)
               callback(data)
               
-      })}}
+      })}
+      
+      resetNotes(callback){
+        return fetch('http://localhost:3000/notes', {
+          method: 'DELETE'
+        })
+        .then(response => response.json())
+        .then(data => {
+            callback(data)
+            
+    })
+      }
+    
+    }
+
+      
+
+
     
 
 
--- notesView.js
+++ notesView.js
@@ -5,6 +5,7 @@ class NotesView{
         this.model = model;
         this.client = client
         this.buttonEl = document.querySelector('#add-note-button');
+        this.resetbuttonEl = document.querySelector('#reset-note-button');
         this.mainContainerEl = document.querySelector('#main-container');
         console.log(this.mainContainerEl)
         this.buttonEl.addEventListener('click', () => {
@@ -13,8 +14,13 @@ class NotesView{
             this.postNote(inputEl);
             inputEl.value = ""
         });
+        this.resetbuttonEl.addEventListener('click', () => {
+            console.log('You have reset notes')
+            this.clearDisplayedNotes()
+            this.resetNotesData()
+        });
     }
-
+    ///takes the array returned from model.getNotes and sets the html to have each note as a div with a claass id of note///
     displayNotes(){
         const notes = this.model.getNotes()
         notes.forEach((note) => {
@@ -25,12 +31,15 @@ class NotesView{
 
         })};
     
+    /// to add note to array without saving to backend
     addNewNote(inputEl){
         this.model.addNote(inputEl.value);
         this.clearDisplayedNotes();
         this.displayNotes();
     };
+    
 
+    ///removes all divs with class note
     clearDisplayedNotes(){
         const allNotes = document.querySelectorAll('div.note')
         allNotes.forEach((element )=> {
@@ -39,12 +48,19 @@ class NotesView{
         })
     };
 
+
+    ///the functions below post/delete and display back end data
+    ///fetches notes array from server, sets the array from the server as the model class array, calls diplay on the model class array
     displayNotesFromApi(){
         return this.client.loadNotes(apiNotes => 
         this.model.setNotes(apiNotes))
             .then(() => this.displayNotes())  
     };
 
+    ///sets the note object to be used as POST argument for createNote client method
+    ///posts a note to the server array with createNote method
+    //sets returned added to server array as the model class array
+    //calls display notes on model aray
     postNote(inputEl){
         const newNote = {content:inputEl.value}
         return this.client.createNote(newNote, apiNotes => 
@@ -52,6 +68,13 @@ class NotesView{
                 .then(() => this.displayNotes()) 
     
 };
-}
+    resetNotesData(){
+        this.client.resetNotes((data) => 
+            this.model.setNotes(data))
+            .then(() => this.displayNotes()) 
+
+        }
+    }
+
 
 module.exports = NotesView
